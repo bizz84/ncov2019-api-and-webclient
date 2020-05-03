efd97ea845c645514cb8475be42d3e450356b66e
bizz84 <bizz84@gmail.com>
2020-05-03 17:55:43 +0100

    Show access token expiration dates

--- web_client/lib/app/dashboard/pages/access_tokens_page.dart
+++ web_client/lib/app/dashboard/pages/access_tokens_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:ncov2019_codewithandrea_web_client/app/dashboard/common_widgets/key_or_token_preview.dart';
 import 'package:ncov2019_codewithandrea_web_client/app/models/environment.dart';
 import 'package:ncov2019_codewithandrea_web_client/app/models/user_authorization_keys_and_tokens.dart';
@@ -25,6 +26,15 @@ class AccessTokensPage extends StatelessWidget {
     }
   }
 
+  String accessTokenTitle(DateTime expirationDate) {
+    if (expirationDate == null) {
+      return 'access token';
+    }
+    final formatter = DateFormat.yMd().add_Hms();
+    final formatted = formatter.format(expirationDate);
+    return 'access token (expires $formatted)';
+  }
+
   @override
   Widget build(BuildContext context) {
     final database = Provider.of<FirestoreDatabase>(context, listen: false);
@@ -36,6 +46,7 @@ class AccessTokensPage extends StatelessWidget {
           final productionAccessToken = keysAndTokens?.productionAccessToken;
           final sandboxKey = keysAndTokens?.sandboxKey;
           final productionKey = keysAndTokens?.productionKey;
+
           return Padding(
             padding: const EdgeInsets.all(16.0),
             child: Column(
@@ -44,7 +55,7 @@ class AccessTokensPage extends StatelessWidget {
                 KeyOrTokenPreview(
                   environment: Environment.sandbox,
                   value: sandboxAccessToken?.accessToken ?? '',
-                  title: 'access token',
+                  title: accessTokenTitle(sandboxAccessToken?.expirationDate),
                   ctaText: 'Reload',
                   isTextVisible: true,
                   onCtaPressed: sandboxKey == null
@@ -56,7 +67,8 @@ class AccessTokensPage extends StatelessWidget {
                 KeyOrTokenPreview(
                   environment: Environment.production,
                   value: productionAccessToken?.accessToken ?? '',
-                  title: 'access token',
+                  title:
+                      accessTokenTitle(productionAccessToken?.expirationDate),
                   ctaText: 'Reload',
                   isTextVisible: true,
                   onCtaPressed: productionKey == null
