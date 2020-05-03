e0d10ec35c84fb72df9a4d29910cf55e5655487c
bizz84 <bizz84@gmail.com>
2020-05-03 20:23:52 +0100

    Add ability to copy to clipboard on web

--- web_client/lib/app/dashboard/common_widgets/key_or_token_preview.dart
+++ web_client/lib/app/dashboard/common_widgets/key_or_token_preview.dart
@@ -1,7 +1,10 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:ncov2019_codewithandrea_web_client/app/dashboard/common_widgets/selectable_text_field.dart';
 import 'package:ncov2019_codewithandrea_web_client/app/models/environment.dart';
 import 'package:ncov2019_codewithandrea_web_client/common_widgets/primary_button.dart';
+import 'package:ncov2019_codewithandrea_web_client/platform_web.dart';
 
 class KeyOrTokenPreview extends StatelessWidget {
   const KeyOrTokenPreview({
@@ -25,9 +28,32 @@ class KeyOrTokenPreview extends StatelessWidget {
         Environment.production: 'Production $title',
       };
 
-  // TODO: Use TextField with obscure argument, disabled input, enabled copy
   String get keyToShow =>
       isTextVisible ? value : '********************************';
+
+  void _copyToClipboard(BuildContext context, String text) {
+    if (_copyToClipboardHack(text)) {
+      Scaffold.of(context)
+          .showSnackBar(SnackBar(content: Text('Copied: $text')));
+    }
+  }
+
+  // https://github.com/flutter/flutter/issues/33470#issuecomment-537802636
+  bool _copyToClipboardHack(String text) {
+    final textarea = TextAreaElement();
+    document.body.append(textarea);
+    textarea.style.border = '0';
+    textarea.style.margin = '0';
+    textarea.style.padding = '0';
+    textarea.style.opacity = '0';
+    textarea.style.position = 'absolute';
+    textarea.readOnly = true;
+    textarea.value = text;
+    textarea.select();
+    final result = document.execCommand('copy');
+    textarea.remove();
+    return result;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -37,16 +63,22 @@ class KeyOrTokenPreview extends StatelessWidget {
         Text(environmentKeyName[environment],
             style: Theme.of(context).textTheme.headline6),
         SizedBox(height: 8),
+        SelectableTextField(
+          text: value,
+          obscured: !isTextVisible,
+        ),
+        SizedBox(height: 16),
         Row(
           mainAxisSize: MainAxisSize.max,
           children: [
-            Expanded(
-              child: SelectableTextField(
-                text: value,
-                obscured: !isTextVisible,
+            Spacer(),
+            if (PlatformWeb.isWeb) ...[
+              PrimaryButton(
+                text: 'Copy',
+                onPressed: () => _copyToClipboard(context, value),
               ),
-            ),
-            SizedBox(width: 16.0),
+              SizedBox(width: 16.0),
+            ],
             PrimaryButton(
               text: ctaText,
               onPressed:
--- web_client/lib/common_widgets/show_alert_dialog.dart
+++ web_client/lib/common_widgets/show_alert_dialog.dart
@@ -2,27 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
-// Querying the environment via [Plattform] throws and exception on Flutter web
-// This extension adds a new [isWeb] getter that should be used
-// before checking for any of the other environments
-extension PlatformWeb on Platform {
-  static bool get isWeb {
-    try {
-      if (Platform.isAndroid ||
-          Platform.isIOS ||
-          Platform.isWindows ||
-          Platform.isFuchsia ||
-          Platform.isLinux ||
-          Platform.isMacOS) {
-        return false;
-      }
-      return true;
-    } catch (e) {
-      return true;
-    }
-  }
-}
+import 'package:ncov2019_codewithandrea_web_client/platform_web.dart';
 
 Future<bool> showAlertDialog({
   @required BuildContext context,
