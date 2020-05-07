1b63df8f85b08c58c2b0efa83132f2a1c462f964
bizz84 <bizz84@gmail.com>
2020-05-07 11:56:46 +0100

    Point API to firebase hosting domain

--- web_client/lib/services/rest_api/api.dart
+++ web_client/lib/services/rest_api/api.dart
@@ -3,8 +3,9 @@ import 'package:ncov2019_codewithandrea_web_client/app/models/endpoint.dart';
 class API {
   //static const String host =
   //    'us-central1-covid19-codewithandrea-dev-api.cloudfunctions.net';
-  static const String host =
-      'us-central1-ncov2019-codewithandrea-api.cloudfunctions.net';
+  //static const String host =
+  //    'us-central1-ncov2019-codewithandrea-api.cloudfunctions.net';
+  static const String host = 'ncov2019-admin.firebaseapp.com';
 
   static Uri tokenUri() => Uri(
         scheme: 'https',
