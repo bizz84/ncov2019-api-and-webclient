070421bb7971f1bcf1203eb68088a1ce1a5b4d14
bizz84 <bizz84@gmail.com>
2020-05-08 11:54:16 +0100

    relative paths for conditional imports in copy_to_clipboard.dart

--- web_client/lib/app/dashboard/common_widgets/copy_to_clipboard.dart
+++ web_client/lib/app/dashboard/common_widgets/copy_to_clipboard.dart
@@ -1,5 +1,5 @@
 import 'copy_to_clipboard_stub.dart'
-    if (dart.library.html) 'package:ncov2019_codewithandrea_web_client/app/dashboard/common_widgets/copy_to_clipboard_web.dart'
-    if (dart.library.io) 'package:ncov2019_codewithandrea_web_client/app/dashboard/common_widgets/copy_to_clipboard_non_web.dart';
+    if (dart.library.html) 'copy_to_clipboard_web.dart'
+    if (dart.library.io) 'copy_to_clipboard_non_web.dart';
 
 bool copyToClipboard(String text) => copyToClipboardImpl(text);
